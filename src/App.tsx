105a47981a4fe6d782f940e5c9cd25a1e8acec13
Vijay Pemmaraju <vijay@grooveapp.com>
2020-06-05 11:17:15 -0700

    Add a simple chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import './App.css';
 
+type DataType = {
+  links: string[],
+  state: string;
+  edit_at: string;
+  city: string;
+  name: string;
+  date: string;
+  date_text: string;
+}
+
+type Point = {
+  x: Date;
+  y: number;
+};
+
 function App() {
+  const [data, setData] = useState<Point[]>([]);
+  useEffect(() => {
+    (async () => {
+      console.log('running effect');
+      const response = await fetch('https://raw.githubusercontent.com/2020PB/police-brutality/data_build/all-locations.json')
+      const json: { data: DataType[] } = await response.json();
+      const transformedData = json.data.reduce((agg, current) => {
+        if (!current.date) {
+          return agg;
+        }
+        const date = new Date(current.date);
+        const existing = agg.find(point => point.x.getTime() === date.getTime());
+        if (existing) {
+          existing.y += 1;
+        } else {
+          agg.push({ x: date, y: 1 })
+        }
+        return agg;
+      }, [] as Point[]);
+      setData(transformedData.map(point => {
+        point.x.setHours(0, 0, 0);
+        return point;
+      }).sort((point1, point2) => point2.x.getTime() - point1.x.getTime()));
+    })()
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.length > 0 && <Line
+        data={{
+          datasets: [{
+            label: 'Incidents',
+            data,
+          }]
+        }}
+        options={{
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day'
+              }
+            }]
+          },
+          maintainAspectRatio: true,
+        }}
+      />}
     </div>
   );
 }
